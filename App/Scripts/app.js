b37424f270dc79a30e9f16f1af77c2892837244f
jaideesukanya <s.jaidee@togetherteam.co.th>
2017-10-03 10:15:35 +0700

    Change some config

--- App/Scripts/app.js
+++ App/Scripts/app.js
@@ -17,8 +17,8 @@ angular.module('catApp', ['ngRoute','AdalAngular'])
     adalProvider.init(
         {
             instance: 'https://login.microsoftonline.com/', 
-            tenant: 'kvaes.onmicrosoft.com',
-            clientId: '9fb35525-a872-4bbd-a973-6165b6ba22aa',
+            tenant: 'together.at',
+            clientId: 'f580ef41-55ef-4edc-be92-06bcd4306c4b',
             extraQueryParameter: 'nux=1',
             //cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
         },
